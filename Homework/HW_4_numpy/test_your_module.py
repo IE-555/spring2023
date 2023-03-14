5a77fbc2750a0e14538854c6d5894c95117debe1
Chase Murray <cmurray3@buffalo.edu>
2023-03-14 15:23:03 -0400

    Added HW 4 (numpy)

---
+++ Homework/HW_4_numpy/test_your_module.py
@@ -0,0 +1,24 @@
+# This python script may be modified/used to test your HW 4 module.
+
+
+import YOURMODULE		# 1)  You'll need to change this.
+
+# 2) You'll need to replace "YOURMODULE" below.
+# 3) The input parameters to the "gradeInfo" function will change when we grade your code.
+#	 For example, a different .csv file might be used, or the other two input parameter values may change.
+[ansA, ansB, ansC, ansD, ansE] = YOURMODULE.gradeInfo('grades_example.csv', 2, 0.3)
+
+print('a:')
+print(ansA)
+
+print('b:')
+print(ansB)
+
+print('c:')
+print(ansC)
+
+print('d:')
+print(ansD)
+
+print('e:')
+print(ansE)
